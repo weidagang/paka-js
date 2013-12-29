3cc88c7e291f6141b220978490cd2e043d8455be
dagang.wei <dagang.wei@ygomi.com>
2013-12-29 23:49:59 +0800

    JSON parser

--- test/json/json-parser.js
+++ test/json/json-parser.js
@@ -10,6 +10,10 @@ function help() {
     console.log('Example: ' + PROGRAM + '{ "k1" : "v1", "k2" : 123, "k3" : false, "k4" : null }');
 }
 
+function escape_str(str) {
+    return str.substring(1, str.length - 1).replace('\\\\', '\\').replace('\\"', '"');
+}
+
 function parse(src) {
     var INT = paka.INT;
     var EOF = paka.EOF;
@@ -19,13 +23,15 @@ function parse(src) {
     var OPT = paka.OPT;
     var DQ_STR = paka.DQ_STR;
     var $ = paka.$;
+    
 
     var grammar = {
-        'JSON' : CONCAT('{', OPT($('KeyValueList')), '}', EOF),
+        'JSON' : CONCAT($('Object'), EOF),
+        'Object' : CONCAT('{', OPT($('KeyValueList')), '}'),
         'KeyValueList' : CONCAT($('KeyValuePair'), REPEAT(CONCAT(',', $('KeyValuePair')), 0)),
         'KeyValuePair' : CONCAT($('Key'), ':', $('Value')),
         'Key' : $('String'),
-        'Value' : OR($('Num'), $('Null'), $('Bool'), $('String')),
+        'Value' : OR($('Num'), $('Null'), $('Bool'), $('String'), $('Object')),
         'Num' : INT(),
         'Null' : 'null',
         'Bool' : OR('false', 'true'),
@@ -33,6 +39,30 @@ function parse(src) {
     };
 
     var action = {
+        'Num' : function(r) { r.extra = parseInt(r.text()); },
+        'Null' : function(r) { r.extra = null },
+        'Bool' : function(r) { r.extra = ('true' == r.text()); },
+        'String' : function(r) { r.extra = escape_str(r.text()); },
+        'Key' : function(r) { },
+        'Value' : function(r) { r.extra = r.children[0].extra; },
+        'KeyValuePair' : function(r) { r.extra = { key : r.children[0].extra, value : r.children[2].extra }; },
+        'KeyValueList' : function(r) { 
+            r.extra = {};
+            r.extra[r.children[0].extra.key] = r.children[0].extra.value;
+            for (var i = 0; i < r.children[1].children.length; ++i) {
+                var kv = r.children[1].children[i].children[1];
+                r.extra[kv.extra.key] = kv.extra.value;
+            }
+        },
+        'Object' : function(r) { 
+            if (null != r.children[1].children && null != r.children[1].children[0])  {
+                r.extra = r.children[1].children[0].extra; 
+            }
+            else {
+                r.extra = {};
+            }
+        },
+        'JSON' : function(r) { r.extra = r.children[0].extra; }
     }
 
     var parser = paka.define(grammar, action);
--- test/json/test-json-parser.js
+++ test/json/test-json-parser.js
@@ -2,18 +2,27 @@
 
 var parser = require('./json-parser.js');
 
+function print_result(ast) {
+    console.log("[Input]:")
+    console.log(json)
+    console.log("")
+    console.log("[Output]:")
+    console.log(ast.status)
+    console.log(ast.extra);
+    console.log("--------------------\n");
+}
+
 var json = '{}'
 var ast = parser.parse(json);
-console.log(json + ' --> ' + ast.status);
 
-var json = '{ "k1" : "v1", "k2" : 1234, "k3" : false, "k4" : null }'
+var json = '{ "k1" : "v1", "k2" : 1234, "k3" : false, "k4" : null, "k5" : { "k 51" : "v 51", "k 52" : 52} }'
 var ast = parser.parse(json);
-console.log(json + ' --> ' + ast.status);
+print_result(ast);
 
 var json = '{ "k 1" : "v 1", "k\\"2" : "v\\"2" }'
 var ast = parser.parse(json);
-console.log(json + ' --> ' + ast.status);
+print_result(ast);
 
 var json = '{ k1 : "v1", "k2" : 1234, "k3" : false, "k4" : null }'
 var ast = parser.parse(json);
-console.log(json + ' --> ' + ast.status);
+print_result(ast);
